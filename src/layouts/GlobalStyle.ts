40be9e6ed944e3d440f6986bfcb2fb4a59aa1bea
Sergey Bunas <Sergey.Bunas@x5.ru>
2021-03-31 00:40:48 +0300

    exercise{11}: fix: font blink, move font faces from GlobalStyle.ts to index.css

--- src/layouts/GlobalStyle.ts
+++ src/layouts/GlobalStyle.ts
@@ -1,43 +1,10 @@
 import { createGlobalStyle } from 'styled-components'
 import reset from 'styled-reset-advanced'
-
-import proximaNovaFont from '../assets/fonts/proxima-nova.woff'
-import proximaNovaFontLight from '../assets/fonts/proxima-nova-light.woff'
-import proximaNovaFontSemiBold from '../assets/fonts/proxima-nova-semibold.woff'
-import proximaNovaFontExtraBold from '../assets/fonts/proxima-nova-extrabold.woff'
 export const GlobalStyle = createGlobalStyle`
   ${reset}
   
   * {
     user-select: text;
-  }
-
-  @font-face {
-    font-family: 'Proxima Nova';
-    src: url(${proximaNovaFont}) format('woff');
-    font-weight: normal;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Proxima Nova';
-    src: url(${proximaNovaFontLight}) format('woff');
-    font-weight: 300;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Proxima Nova';
-    src: url(${proximaNovaFontSemiBold}) format('woff');
-    font-weight: 600;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Proxima Nova';
-    src: url(${proximaNovaFontExtraBold}) format('woff');
-    font-weight: 800;
-    font-style: normal;
   }
 
   html {
