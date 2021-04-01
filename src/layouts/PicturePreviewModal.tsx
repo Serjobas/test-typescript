5ed76253345243ffa4b767d857f612cbda19e829
Sergey Bunas <Sergey.Bunas@x5.ru>
2021-04-01 17:50:12 +0300

    exercise{7}: add: landscape mode to PicturePreviewModal.tsx based on ratio

--- src/layouts/PicturePreviewModal.tsx
+++ src/layouts/PicturePreviewModal.tsx
@@ -3,22 +3,33 @@ import { useCallback, useContext } from 'react'
 import { PictureModalContext } from '../components/PictureModalContext/context'
 import { Modal } from '../components/Modal'
 import { ASSETS_ENDPOINT } from '../api/config'
-import styled from 'styled-components/macro'
+import styled, { css } from 'styled-components/macro'
 import { H3, PrimaryText } from 'src/components/typography'
 
-const Content = styled.div`
+const Content = styled.div<{ isLandscape?: boolean }>`
   height: 100%;
-  display: flex;
-  justify-content: space-evenly;
-  flex-direction: row;
+  width: 100%;
+  display: inline-flex;
+  justify-content: ${({ isLandscape }) => (isLandscape ? 'flex-start' : 'space-evenly')};
+  flex-direction: ${({ isLandscape }) => (isLandscape ? 'column' : 'row')};
 `
 
-const Column = styled.div`
-  height: 100%;
+const Column = styled.div<{ isImageColumn?: boolean; isLandscape?: boolean }>`
+  height: ${({ isImageColumn, isLandscape }) => (isLandscape && isImageColumn ? '75%' : '90%')};
+  width: ${({ isLandscape }) => (isLandscape ? '100%' : 'auto')};
+  text-align: center;
 `
-const Image = styled.img`
-  //width: 100%;
-  height: 100%;
+const Image = styled.img<{ isLandscape?: boolean }>`
+  ${({ isLandscape }) =>
+    isLandscape
+      ? css`
+          height: 100%;
+          max-width: 100%;
+        `
+      : css`
+          max-width: 100%;
+          height: 100%;
+        `};
 `
 export const PicturePreviewModal: React.FC = React.memo(() => {
   const { openedPicture, openPicture } = useContext(PictureModalContext)
@@ -29,13 +40,15 @@ export const PicturePreviewModal: React.FC = React.memo(() => {
 
   if (!openedPicture) return null
 
+  const isLandscape = openedPicture.width / openedPicture.height >= 1.5
+
   return (
     <Modal onClose={handleClose}>
-      <Content>
-        <Column>
-          <Image src={ASSETS_ENDPOINT + openedPicture.urls.regular} />
+      <Content isLandscape={isLandscape}>
+        <Column isImageColumn isLandscape={isLandscape}>
+          <Image isLandscape={isLandscape} src={ASSETS_ENDPOINT + openedPicture.urls.regular} />
         </Column>
-        <Column>
+        <Column isLandscape={isLandscape}>
           <H3>Picture info</H3>
           <hr />
           <p>
@@ -43,7 +56,7 @@ export const PicturePreviewModal: React.FC = React.memo(() => {
           </p>
           <p>
             <PrimaryText>
-              Dimensions: {openedPicture.width}px&times;{openedPicture.height}px
+              Dimensions: {openedPicture.width}px&times;{openedPicture.height}px ratio{' '}
             </PrimaryText>
           </p>
           <p>
